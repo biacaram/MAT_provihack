ce7db1eea4b4b1ad5a0a18084bb76bd822378007
beatriz andre <biacaram@hotmail.com>
2021-02-06 18:30:48 -0300

    vagas model controller

--- db/migrate/20210206205013_create_vagas.rb
+++ db/migrate/20210206205013_create_vagas.rb
@@ -1,6 +1,9 @@
 class CreateVagas < ActiveRecord::Migration[6.1]
   def change
     create_table :vagas do |t|
+      t.string :nome
+      t.text :skills
+      t.references :empresa, null: false, foreign_key: true
 
       t.timestamps
     end
