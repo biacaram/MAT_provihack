780a7c790fc7d5d6ae91cf446a851eba6bd367e0
beatriz andre <biacaram@hotmail.com>
2021-02-06 20:51:24 -0300

    usuario controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'usuarios/index'
+  get 'usuarios/new'
+  get 'usuarios/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#home"
   resources :empresas, only: [ :index, :new, :create, :show ] do
