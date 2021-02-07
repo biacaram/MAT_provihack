5da39ced9808a6ddcdd6ce6c481227bb80f45f59
beatriz andre <biacaram@hotmail.com>
2021-02-06 21:11:59 -0300

    txt pra salvar blocos

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'usuarios/index'
-  get 'usuarios/new'
-  get 'usuarios/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#home"
+  resources :usuarios, only: [ :index, :new, :create ]
   resources :empresas, only: [ :index, :new, :create, :show ] do
     resources :vagas, only: [ :index, :new, :create]
   end
